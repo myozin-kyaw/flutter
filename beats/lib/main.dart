99f5bd94b8296dc63300c66df24ed03d2975bbca
Myo Zin Kyaw <108007637+myozin-kyaw@users.noreply.github.com>
2024-03-27 18:53:25 +0630

    music app icon adding

--- beats/lib/main.dart
+++ beats/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Beats',
+      title: 'Music Player',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
